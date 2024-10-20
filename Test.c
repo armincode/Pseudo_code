800ec61bd4cdef131567b6b9ef9347c3cd2e495d
armincode <65513658+armincode@users.noreply.github.com>
2024-10-20 16:32:24 +0330

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,29 +1,39 @@
 WHILE (process_not_done = True) {
     // Step 1. check if blockhash is still Valid
+    
     IF (getBlockHeight() <= lastValidBlockHeight) {    // this condition is simply how Solana checks for valid blockhashes
-        // Step 2. Check Transaction Status
-        status = getSignatureStatuses(transactionSignature);                //get status to determine if it's already on the network
+        // Step 2. check Transaction Status
+        status = getSignatureStatuses(TX id);                //get status to determine if it's already on the chain
 
-        IF (status == "finalized") {
-            // TX is finalized, exit loop (task accomplished)
+        
+        IF (status == "finalized") 
+             /* TX is finalized, exit loop. (task accomplished but shouldn't use finalized commitment when sending TXs, 
+            because "finalized" returns an older blockhash, reducing the available time before the blockhash expires, which could cause more failures, especially if network is busy. 
+            But to check for absolute security and final confirmation, you can check for finalized status after the transaction is processed.) */
             break;  
-        } ELSE IF (status == "confirmed") {
-            // Transaction is confirmed but not yet finalized, optional retry or wait (safe to consider task accomplished)
-            wait(some delay);
-        } ELSE {
-            // Transaction is neither confirmed or finalized, Retry sending:
+        
+           
+        ELSE IF (status == "confirmed") 
+            // Transaction is confirmed but not yet finalized, optional retry or wait (usually safe to consider task accomplished)
+            wait(some delay); 
+  
+    }
+
+         ELSE {
+            // here Transaction is neither confirmed or finalized, so Retry Sending the Transaction until Accepted:
             try {
-                submitTransaction(transaction);
-                wait(some delay);  // Optional delay between retries
-            } catch (error) {
+                sendTransaction(transaction);
+                wait(some delay);  // It's better to specify delay between retries
+             catch (error) {
                 // handle network errors or transaction rejections
                 log(error);
+                }
             }
-        }
-    } ELSE {
-        // Step 3. blockhash is Expired, Fetch New Blockhash and Resign Transaction
+         }
+     ELSE {
+        // Step 3. blockhash is expired, Fetch new blockhash and Resign Transaction
         { blockhash, lastValidBlockHeight } = getRecentBlockhash();
         transaction.recentBlockhash = blockhash;
         signTransaction(transaction);  // Re-sign the transaction with the new blockhash
-    }
+
 }
